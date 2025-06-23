1661e346efddb6546a7bcd484ed505b872849274
zalesol265 <zalesol265@gmail.com>
2025-06-23 14:25:01 -0400

    Add warning message for overflow

--- MMM-RecipeReader.js
+++ MMM-RecipeReader.js
@@ -23,6 +23,78 @@ Module.register("MMM-RecipeReader", {
     return txt.value;
   },
 
+  // getDom() {
+  //   const wrapper = document.createElement("div");
+  //   wrapper.className = "MMM-RecipeReader-wrapper";
+  
+  //   if (!this.recipe) {
+  //     wrapper.innerHTML = "<i>Waiting for recipe...</i>";
+  //     return wrapper;
+  //   }
+  
+  //   const { title, image, ingredients, instructions } = this.recipe;
+  
+  //   const scrollContainer = document.createElement("div");
+  //   scrollContainer.className = "MMM-RecipeReader-scrollContainer";
+  //   scrollContainer.appendChild(topContainer);
+  //   scrollContainer.appendChild(instr);
+
+  //   // Top container
+  //   const topContainer = document.createElement("div");
+  //   topContainer.className = "MMM-RecipeReader-top";
+  
+  //   const titleIng = document.createElement("div");
+  //   // Title
+  //   const titleEl = document.createElement("h2");
+  //   titleEl.className = "MMM-RecipeReader-title";
+  //   titleEl.innerText = title;
+  //   titleIng.appendChild(titleEl);
+      
+  //   // Ingredients
+  //   const ingList = document.createElement("ul");
+  //   ingList.className = "MMM-RecipeReader-ingredients";
+  //   ingredients.forEach((i) => {
+  //     const li = document.createElement("li");
+  //     li.innerText = this.decodeHtmlEntities(i);
+  //     ingList.appendChild(li);
+  //   });
+  //   titleIng.appendChild(ingList);
+
+  //   topContainer.appendChild(titleIng);
+
+  //   // Image
+  //   if (image) {
+  //     const img = document.createElement("img");
+  //     img.src = image;
+  //     img.className = "MMM-RecipeReader-image";
+  //     topContainer.appendChild(img);
+  //   }
+  //   // wrapper.appendChild(topContainer);
+  
+  //   // Instructions
+  //   const instr = document.createElement("ol");
+  //   instr.className = "MMM-RecipeReader-instructions";
+  //   instructions.forEach((i) => {
+  //     const li = document.createElement("li");
+  //     li.innerText = i;
+  //     instr.appendChild(li);
+  //   });
+  //   // wrapper.appendChild(instr);
+
+  //   // Overflow warning message
+  //   const overflowMsg = document.createElement("div");
+  //   overflowMsg.className = "MMM-RecipeReader-overflowMsg";
+  //   overflowMsg.style.display = "none"; // hidden by default
+  //   overflowMsg.innerText = "The display cannot show the full recipe.";
+
+  //   scrollContainer.appendChild(topContainer);
+  //   scrollContainer.appendChild(instr);
+  //   wrapper.appendChild(overflowMsg);
+  //   wrapper.appendChild(scrollContainer);
+
+  //   return wrapper;
+  // },
+
   getDom() {
     const wrapper = document.createElement("div");
     wrapper.className = "MMM-RecipeReader-wrapper";
@@ -34,19 +106,17 @@ Module.register("MMM-RecipeReader", {
   
     const { title, image, ingredients, instructions } = this.recipe;
   
-
-    // Top container
+    // Create top container (title + ingredients + image)
     const topContainer = document.createElement("div");
     topContainer.className = "MMM-RecipeReader-top";
   
     const titleIng = document.createElement("div");
-    // Title
+  
     const titleEl = document.createElement("h2");
     titleEl.className = "MMM-RecipeReader-title";
     titleEl.innerText = title;
     titleIng.appendChild(titleEl);
-      
-    // Ingredients
+  
     const ingList = document.createElement("ul");
     ingList.className = "MMM-RecipeReader-ingredients";
     ingredients.forEach((i) => {
@@ -55,19 +125,17 @@ Module.register("MMM-RecipeReader", {
       ingList.appendChild(li);
     });
     titleIng.appendChild(ingList);
-
+  
     topContainer.appendChild(titleIng);
-
-    // Image
+  
     if (image) {
       const img = document.createElement("img");
       img.src = image;
       img.className = "MMM-RecipeReader-image";
       topContainer.appendChild(img);
     }
-    wrapper.appendChild(topContainer);
   
-    // Instructions
+    // Create instructions
     const instr = document.createElement("ol");
     instr.className = "MMM-RecipeReader-instructions";
     instructions.forEach((i) => {
@@ -75,8 +143,29 @@ Module.register("MMM-RecipeReader", {
       li.innerText = i;
       instr.appendChild(li);
     });
-    wrapper.appendChild(instr);
-    console.log(wrapper);
+  
+    // Create scroll container
+    const scrollContainer = document.createElement("div");
+    scrollContainer.className = "MMM-RecipeReader-scrollContainer";
+    scrollContainer.appendChild(topContainer);
+    scrollContainer.appendChild(instr);
+  
+    // Overflow warning message
+    const overflowMsg = document.createElement("div");
+    overflowMsg.className = "MMM-RecipeReader-overflowMsg";
+    overflowMsg.style.display = "none";
+    overflowMsg.innerText = "The display cannot show the full recipe.";
+  
+    wrapper.appendChild(overflowMsg);
+    wrapper.appendChild(scrollContainer);
+  
+    // Check overflow
+    setTimeout(() => {
+      if (scrollContainer.scrollHeight > scrollContainer.clientHeight) {
+        overflowMsg.style.display = "block";
+      }
+    }, 100);
+  
     return wrapper;
   },
 
